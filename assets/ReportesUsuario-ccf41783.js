4112eb2e20e8f8941af96b5fc4da48cdcfa0ca33
JL <jloo6778@gmail.com>
2023-12-20 17:08:59 -0500

    cambio ruta de 1 punto

--- assets/ReportesUsuario-ccf41783.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,d as r}from"./index-310ea401.js";const t={};function c(s,n){return o(),r("h1",null,"Reportes")}const f=e(t,[["render",c]]);export{f as default};
