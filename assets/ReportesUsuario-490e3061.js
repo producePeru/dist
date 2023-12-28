8c40edcfd714daf47e092f1380417730d08c093a
JL <jloo6778@gmail.com>
2023-12-28 09:54:24 -0500

    size mype

--- assets/ReportesUsuario-490e3061.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,d as r}from"./index-2981fb11.js";const t={};function c(s,n){return o(),r("h1",null,"Reportes")}const f=e(t,[["render",c]]);export{f as default};
