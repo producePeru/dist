1e6a5828803bb4d0412633c55a3cf1e5a4124dbb
JL <jloo6778@gmail.com>
2024-03-12 17:59:29 -0500

    formalizaciones

--- assets/SuccessMessage-60c13c0d.js
+++
@@ -1,1 +0,0 @@
-import{x as u,b as o,o as a,B as r,i as e}from"./index-0c4a2b29.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
