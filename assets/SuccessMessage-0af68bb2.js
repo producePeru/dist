b8af4aba3daba23bb55a219a799e77e99d9fb1a6
JL <jloo6778@gmail.com>
2024-03-25 11:10:55 -0500

    fd

--- assets/SuccessMessage-0af68bb2.js
+++
@@ -1,1 +0,0 @@
-import{z as u,d as o,o as a,v as r,j as e}from"./index-8a2a9bae.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
