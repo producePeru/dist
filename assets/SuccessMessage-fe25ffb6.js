b42fffd67e57cc21c1e6ba5b39dd568cb4609e14
JL <jloo6778@gmail.com>
2024-03-26 16:59:41 -0500

    buscador

---
+++ assets/SuccessMessage-fe25ffb6.js
@@ -0,0 +1,1 @@
+import{y as u,d as o,o as a,s as r,i as e}from"./index-24dd0db0.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
