2a650a1a90f747746b395e6cddb237b5ba34f12c
HP PRODUCE <produce@gmail.com>
2024-04-12 14:39:18 -0500

    wow

--- assets/InicioDashboard-9797d0b6.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,d as r,e as t}from"./index-e61e75d6.js";const c={},i=t("iframe",{src:"https://app.powerbi.com/view?r=eyJrIjoiNTBkNDY1OGUtMTdlZi00MjExLTg5YmEtODRiYThjOTAzZjc3IiwidCI6Ijc1MDRlMzE4LThlMWUtNGQ1NS1iZmZkLTg3NWI0ZGVlODI2MCIsImMiOjR9",frameborder:"0",allowFullScreen:"true"},`\r
-    `,-1),s=[i];function a(n,_){return o(),r("div",null,s)}const m=e(c,[["render",a]]);export{m as default};
