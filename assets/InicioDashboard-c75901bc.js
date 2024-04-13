bd03e2bc17419fb28de0fbc1a71f1e30fa931732
JL <jloo6778@gmail.com>
2024-04-13 08:39:31 -0500

    wow

---
+++ assets/InicioDashboard-c75901bc.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,d as r,e as t}from"./index-7a9e279b.js";const c={},i=t("iframe",{src:"https://app.powerbi.com/view?r=eyJrIjoiNTBkNDY1OGUtMTdlZi00MjExLTg5YmEtODRiYThjOTAzZjc3IiwidCI6Ijc1MDRlMzE4LThlMWUtNGQ1NS1iZmZkLTg3NWI0ZGVlODI2MCIsImMiOjR9",frameborder:"0",allowFullScreen:"true"},`\r
+    `,-1),s=[i];function a(n,_){return o(),r("div",null,s)}const m=e(c,[["render",a]]);export{m as default};
