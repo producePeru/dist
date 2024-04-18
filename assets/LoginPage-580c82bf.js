3e75c4ce9d15546b2a72506aad6fa8512cffaf6c
HP PRODUCE <produce@gmail.com>
2024-04-18 10:06:10 -0500

    wow

--- assets/LoginPage-580c82bf.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,c}from"./index-6912df85.js";const r={};function n(t,a){return o(),c("span")}const f=e(r,[["render",n]]);export{f as default};
--- assets/SuccessMessage-27f175cb.js
+++
@@ -1,1 +0,0 @@
-import{x as u,h as o,o as a,i as r,u as e}from"./index-6912df85.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
