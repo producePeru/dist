c06f06339ed8a0b774a3ee68ab4f75ca2a744faf
JL <jloo6778@gmail.com>
2024-05-06 18:51:50 -0500

    asesorias

--- assets/SuccessMessage-f6d193d7.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:assets/SuccessMessage-f6d193d7.js
-import{x as u,b as o,o as a,q as r,h as e}from"./index-9cb3fc67.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
-========
-import{x as u,b as o,o as a,q as r,h as e}from"./index-9211c6e9.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
->>>>>>>> 65cff89ef98c58b72f257ba662622b92a48dd94d:assets/SuccessMessage-baa3efa1.js
--- assets/Unauthorized-fb737cc9.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:assets/Unauthorized-fb737cc9.js
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,d as r}from"./index-9cb3fc67.js";const t={};function n(c,a){return o(),r("h1",null,"unauthorized")}const f=e(t,[["render",n]]);export{f as default};
-========
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,d as r}from"./index-9211c6e9.js";const t={};function n(c,a){return o(),r("h1",null,"unauthorized")}const f=e(t,[["render",n]]);export{f as default};
->>>>>>>> 65cff89ef98c58b72f257ba662622b92a48dd94d:assets/Unauthorized-82fc9457.js
