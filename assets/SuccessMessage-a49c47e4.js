50185c48acfc9b1d0e06aa9d826794900aaf85f0
HP PRODUCE <produce@gmail.com>
2024-10-22 12:13:50 -0500

    correcciones de fechas

--- assets/SuccessMessage-a49c47e4.js
+++
@@ -1,1 +0,0 @@
-import{z as u,b as o,o as a,s as r,h as e}from"./index-af55d7e2.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
