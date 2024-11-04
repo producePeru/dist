600e137a659613d98fe1144618f359f9fb359bce
HP PRODUCE <produce@gmail.com>
2024-11-04 09:10:28 -0500

    ferias

---
+++ assets/SuccessMessage-f6e4126d.js
@@ -0,0 +1,1 @@
+import{G as u,b as o,o as a,s as r,h as e}from"./index-3b90dc86.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
