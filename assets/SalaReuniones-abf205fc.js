22c05f0241d5dba722619a795a921c8a2cd20767
David <quispesistema@gmail.com>
2025-01-09 13:46:01 -0500

    correos

--- assets/SalaReuniones-abf205fc.js
+++
@@ -1,1 +0,0 @@
-import{V as l}from"./main-0c6046f3.js";import{r,o as n,p as s,h as t}from"./index-a1472395.js";const d={__name:"SalaReuniones",setup(u){const e=r();return(m,a)=>(n(),s(t(l),{modelValue:e.value,"onUpdate:modelValue":a[0]||(a[0]=o=>e.value=o),locale:"es"},null,8,["modelValue"]))}};export{d as default};
