8ccf76ee0ec48cdcda38ce408ab427dd46a0d692
David <quispesistema@gmail.com>
2025-01-10 11:53:09 -0500

    test

--- assets/SalaReuniones-e7ea9e72.js
+++
@@ -1,1 +0,0 @@
-import{V as l}from"./main-d26b4a06.js";import{r,o as n,p as s,h as t}from"./index-3723526f.js";const d={__name:"SalaReuniones",setup(u){const e=r();return(m,a)=>(n(),s(t(l),{modelValue:e.value,"onUpdate:modelValue":a[0]||(a[0]=o=>e.value=o),locale:"es"},null,8,["modelValue"]))}};export{d as default};
