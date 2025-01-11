23aa0ec1f5664bfe4ba1c87e1793b3eee6549578
David <quispesistema@gmail.com>
2025-01-10 20:11:10 -0500

    fix: remove unused import of ref in EnvioCorreosMasivos.vue

---
+++ assets/SalaReuniones-53d68094.js
@@ -0,0 +1,1 @@
+import{V as l}from"./main-38f5db81.js";import{r,o as n,p as s,h as t}from"./index-518b448a.js";const d={__name:"SalaReuniones",setup(u){const e=r();return(m,a)=>(n(),s(t(l),{modelValue:e.value,"onUpdate:modelValue":a[0]||(a[0]=o=>e.value=o),locale:"es"},null,8,["modelValue"]))}};export{d as default};
