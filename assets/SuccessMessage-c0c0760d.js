513c3951dedb67e90b1a6bcadbeb25031df11566
David <quispesistema@gmail.com>
2025-01-19 18:14:22 -0500

    asesores nuevo

---
+++ assets/SuccessMessage-c0c0760d.js
@@ -0,0 +1,1 @@
+import{H as u,b as o,o as a,v as r,h as e}from"./index-55e5e582.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
