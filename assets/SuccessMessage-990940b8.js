e4c3de57cd724d0fbb1ed8b344fc2015d10f6038
David <quispesistema@gmail.com>
2025-02-16 22:18:07 -0500

    qr

---
+++ assets/SuccessMessage-990940b8.js
@@ -0,0 +1,1 @@
+import{z as u,b as o,o as a,x as r,h as e}from"./index-e53dd249.js";const i={__name:"SuccessMessage",setup(l){const t=u();return(n,c)=>{const s=o("a-result");return a(),r(s,{status:e(t).query.status,title:e(t).query.title,"sub-title":e(t).query.subTitle},null,8,["status","title","sub-title"])}}};export{i as default};
