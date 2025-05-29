6f80964829c8f8210afe9c004e349d7aad4f3cb3
David Richard Quispe Conejo - O/S <tuempresa_temp367@produce.gob.pe>
2025-05-29 10:23:49 -0500

    app ferias

--- assets/SedIndex-93e31a22.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,d as r}from"./index-ad80a480.js";const c={};function n(t,s){return o(),r("h1",null,"SED")}const f=e(c,[["render",n]]);export{f as default};
