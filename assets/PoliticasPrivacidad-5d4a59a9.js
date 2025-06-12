090aceaac82318509ed09fd275729cbad739dfaa
David Quispe <jloo6778@gmail.com>
2025-06-12 18:36:49 -0500

    app

---
+++ assets/PoliticasPrivacidad-5d4a59a9.js
@@ -0,0 +1,47 @@
+import{_ as y,a as S}from"./logo-tuempresa-c1197180.js";import{F as A}from"./FormalizacionFooter-3cdf9721.js";import{_ as T}from"./_plugin-vue_export-helper-c27b6911.js";import{r as s,j as q,k as z,o as M,d as R,e,n as U,t as o,c as j,F as x}from"./index-f8d1f2b5.js";import"./EnvironmentOutlined-8512d906.js";const D={class:"form-x politicas"},N={class:"container"},C={class:"form-x-wrapper ff"},L=`TU EMPRESA recopila datos personales por medio del registro que usted realiza
+en el formulario, los mismos que están referidos a: `,w=`Esta información servirá para cumplir con las finalidades detalladas más adelante y
+relacionadas al Servicio.`,F=`Además de los anteriores derechos, el usuario tendrá derecho a retirar el
+consentimiento otorgado en cualquier momento mediante el procedimiento descrito
+líneas arriba, sin que el retiro del consentimiento afecte la licitud del tratamiento
+anterior al retiro del mismo.)..`,I={__name:"PoliticasPrivacidad",setup(_){const r=s(`El Programa Nacional “Tu Empresa”, en adelante TU EMPRESA, respeta la legislación
+vigente en materia de protección de datos personales, la privacidad de los ciudadanos,
+el secreto y seguridad de la información concerniente a las personas, de conformidad
+en lo previsto en el artículo 2 numeral 6 de la Constitución Política y lo establecido en la
+Ley Nº 29733, Ley de Protección de datos personales, y su reglamento aprobado con
+Decreto Supremo N° 003-2013-JUS, adoptando para ello las medidas técnicas y
+organizativas necesarias para evitar la pérdida, mal uso, alteración, acceso no
+autorizado y robo de los datos personales facilitados, habida cuenta del estado de la
+tecnología, la naturaleza de los datos y los riesgos a los que están expuestos.`),t=s(`La presente Política tiene como objetivo establece las disposiciones necesarias
+para garantizar la seguridad de la información contenida en la base de datos de TU
+EMPRESA, basado en el cumplimiento de la presente política de privacidad.`),d=s(`El presente Política de Privacidad del servicio digital denominado “CREA TU
+EMPRESA”, se encuentra a cargo y es de responsabilidad de TU EMPRESA.`),c=s(`Cabe recalcar que TU EMPRESA asegura la máxima reserva y protección sobre
+aquellos datos personales de los usuarios que utilicen el Servicio.`),u=s(`TU EMPRESA tratará sus datos personales con la finalidad de gestionar el presente
+Servicio en ejercicio de las funciones públicas conferidas y siempre en el marco de
+sus competencias.`),p=s(`Los datos personales no podrán ser utilizados para otras finalidades no informadas
+y que no guarden relación con el presente Servicio.`),m=s(`No se encuentra prevista la transferencia de sus datos personales a terceros, salvo
+para el cumplimiento del presente Servicio y cuando se trate de una obligación legal
+a petición de un organismo público o autoridad judicial.`),v=s(`Los datos serán almacenados en los servidores de Amazon Web Services siendo
+encargados de esta gestión la Presidencia del Consejo de Ministros (PCM) a través
+de la Secretaría de Gobierno y Transformación Digital. En estos casos, la Entidad
+garantiza que el tratamiento de sus datos se limite a las finalidades antes
+autorizadas, que se mantengan de forma confidencial, así como que se
+implementen las medidas de seguridad que exige la Ley de Protección de Datos
+personales, su Reglamento y los Lineamientos para Uso de Servicios en la Nube
+para Entidades de la Administración Pública del Estado Peruano.
+`),g=s(`TU EMPRESA realizará tratamiento sobre datos personales obtenidos con el
+consentimiento previo, expreso e informado del titular de los datos personales.`),f=s(`Al aceptar esta Política de Privacidad, el usuario presta su consentimiento para el
+tratamiento de datos personales para las finalidades expuestas.`),P=s(`Los datos personales tratados serán almacenados en el banco de datos personales
+denominado “Usuarios de los servicios del Programa Nacional “Tu Empresa” de
+titularidad de TU EMPRESA. Asimismo, TU EMPRESA conserva sus datos
+personales durante el tiempo imprescindible y necesario para gestionar el presente
+Servicio y otros relacionados al mismo, sin perjuicio de que los usuarios puedan
+revocar el consentimiento en cualquier momento.`),l=s(`En cumplimiento de la normativa vigente, TU EMPRESA y las entidades
+involucradas en el tratamiento han adoptado medidas legales, organizativas y
+técnicas apropiadas para garantizar la seguridad de los datos personales, evitando
+su alteración, pérdida, tratamiento indebido o acceso no autorizado que puedan
+afectar a la integridad, confidencialidad disponibilidad de datos personales.`),E=s(`TU EMPRESA se reserva el derecho a modificar su Política de Privacidad en el
+supuesto de que exista un cambio en la legislación vigente, doctrinal,
+jurisprudencial. TU EMPRESA podrá realizar modificaciones y correcciones a esta
+Política de Privacidad. Si se introdujera algún cambio en esta Política, el nuevo texto
+se publicará y estará a disposición en este mismo formulario o a solicitud de los
+interesados por cualquier medio.`),b=s(null),i=s(!1),n=()=>{window.pageYOffset>10?i.value=!0:i.value=!1};return q(()=>{window.addEventListener("scroll",n)}),z(()=>{window.removeEventListener("scroll",n)}),(B,a)=>(M(),R(x,null,[e("header",{class:U(["header",{floatingNav:i.value}]),ref_key:"navBar",ref:b},a[0]||(a[0]=[e("div",{class:"container header-wrapper"},[e("img",{src:y,alt:"logo-produce"}),e("img",{src:S,alt:"logo-tu-empresa"})],-1)]),2),e("section",D,[e("div",N,[e("div",C,[a[13]||(a[13]=e("h2",{class:"ff title"},"POLÍTICA DE PRIVACIDAD – CREA TU EMPRESA",-1)),e("p",null,o(r.value),1),e("ol",null,[e("div",null,[a[1]||(a[1]=e("li",{class:"title-2"},"Objetivo",-1)),e("p",null,o(t.value),1)]),e("div",null,[a[2]||(a[2]=e("li",{class:"title-2"}," Identidad y domicilio:",-1)),e("p",null,o(d.value),1),e("p",null,o(c.value),1)]),e("div",null,[a[3]||(a[3]=e("li",{class:"title-2"},"Datos personales recopilados:",-1)),e("p",null,o(L)),a[4]||(a[4]=e("ul",{style:{margin:"1rem 0 1rem 3rem"}},[e("li",null,"Nombres y apellidos "),e("li",null,"Número de DNI u otro documento de identidad"),e("li",null,"Teléfono de contacto"),e("li",null,"Domicilio"),e("li",null,"Correo electrónico")],-1)),e("p",null,o(w))]),e("div",null,[a[5]||(a[5]=e("li",{class:"title-2"},"Finalidad del tratamiento de los datos personales ",-1)),e("p",null,o(u.value),1),e("p",null,o(p.value),1)]),e("div",null,[a[6]||(a[6]=e("li",{class:"title-2"},"Transferencia",-1)),e("p",null,o(m.value),1),e("p",null,o(v.value),1)]),e("div",null,[a[7]||(a[7]=e("li",{class:"title-2"},"Consentimiento",-1)),e("p",null,o(g.value),1),e("p",null,o(f.value),1)]),e("div",null,[a[8]||(a[8]=e("li",{class:"title-2"},"Plazo de tratamiento de los datos personales",-1)),e("p",null,o(P.value),1)]),e("div",null,[a[9]||(a[9]=e("li",{class:"title-2"},"Seguridad de los Datos Personales",-1)),e("p",null,o(l.value),1)]),e("div",null,[a[10]||(a[10]=e("li",{class:"title-2"},"Sobre el ejercicio de derechos como titular de datos personales",-1)),e("p",null,o(l.value),1),e("p",null,o(F)),a[11]||(a[11]=e("p",null,"De considerar que no ha sido atendido en el ejercicio de sus derechos puede presentar una reclamación ante la Autoridad Nacional de Protección de Datos Personales dirigiéndose al correo electrónico protegetusdatos@minjus.gob.pe. ",-1))]),e("div",null,[a[12]||(a[12]=e("li",{class:"title-2"},"Vigencia y Modificación de la Política de Privacidad",-1)),e("p",null,o(E.value),1)])])])])]),j(A)],64))}},W=T(I,[["__scopeId","data-v-276c73ad"]]);export{W as default};
