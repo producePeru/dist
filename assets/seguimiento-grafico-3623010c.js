81fd2430dbbb97fd4542a95e3619e308b56486ab
Elizabeth Holmes <eliza@pnte.com>
2025-09-12 15:24:12 -0500

    modulo de las capacitaciones

---
+++ assets/seguimiento-grafico-3623010c.js
@@ -0,0 +1,1 @@
+import{a as e}from"./PointStyling-5b85601c.js";import{o,d as r,c as t}from"./index-8ef95d84.js";import"./_plugin-vue_export-helper-c27b6911.js";const p={__name:"seguimiento-grafico",setup(a){return(c,n)=>(o(),r("div",null,[t(e)]))}};export{p as default};
