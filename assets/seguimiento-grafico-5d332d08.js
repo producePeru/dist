510b62ff002494d21ce62b218871b2e6501b295e
Elizabeth Holmes <eliza@pnte.com>
2025-09-16 12:30:55 -0500

    mejoras en las capacitaciones y ppjj

---
+++ assets/seguimiento-grafico-5d332d08.js
@@ -0,0 +1,1 @@
+import{C as e}from"./PointStyling-21b35582.js";import{o,d as r,c as t}from"./index-d32b4ca9.js";import"./auto-ba4ecebb.js";import"./_plugin-vue_export-helper-c27b6911.js";const _={__name:"seguimiento-grafico",setup(a){return(c,n)=>(o(),r("div",null,[t(e)]))}};export{_ as default};
