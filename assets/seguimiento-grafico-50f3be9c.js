fd8b5616d8c5f2c2b4a273eeff85ab63779e9837
Elizabeth Holmes <eliza@pnte.com>
2025-10-10 16:42:39 -0500

    actividades ugo template

--- assets/seguimiento-grafico-50f3be9c.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./PointStyling-20556b99.js";import{o,d as r,c as t}from"./index-1a16f601.js";import"./auto-ba4ecebb.js";import"./_plugin-vue_export-helper-c27b6911.js";const _={__name:"seguimiento-grafico",setup(a){return(c,n)=>(o(),r("div",null,[t(e)]))}};export{_ as default};
