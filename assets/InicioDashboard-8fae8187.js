856dd769b6b0dabdcd51eec4e54020129238ed3a
Developer <developer_factory@tuempresa.com>
2026-06-30 12:11:38 -0500

    ugsc

---
+++ assets/InicioDashboard-8fae8187.js
@@ -0,0 +1,1 @@
+import{o as e,c as o}from"./index-2fee1da9.js";const n={__name:"InicioDashboard",setup(c){return(t,a)=>(e(),o("section"))}};export{n as default};
--- assets/NoFound-04f0a7d8.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-c27b6911.js";import{o as n,c as t,d as s}from"./index-96903144.js";const _={},r={class:"fof"};function c(a,e){return n(),t("div",r,e[0]||(e[0]=[s("h1",null,"Página 404",-1)]))}const l=o(_,[["render",c]]);export{l as default};
