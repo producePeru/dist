bd0a11bb6b89605f8a126b53cb88f4d2f159269a
Elizabeth Holmes <eliza@pnte.com>
2026-08-20 17:53:25 -0500

    feat: implement CORS configuration and add ActividadPnteController for activity management

---
+++ assets/InicioDashboard-edc6ceb9.js
@@ -0,0 +1,1 @@
+import{o as e,c as o}from"./index-6d093441.js";const n={__name:"InicioDashboard",setup(c){return(t,a)=>(e(),o("section"))}};export{n as default};
---
+++ assets/PlanesAccion-3b3da405.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as c,c as o}from"./index-6d093441.js";const n={};function r(t,s){return c(),o("div",null," Hello ")}const _=e(n,[["render",r]]);export{_ as default};
